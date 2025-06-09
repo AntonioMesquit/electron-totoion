56800623e0afbe3a1ef8fc6b737f5b7beed6b0d5
playerones2 <140577539+AntonioMesquit@users.noreply.github.com>
2025-06-08 21:27:17 -0300

    feat: criação pagina estatica de documento

--- src/renderer/src/pages/document.tsx
+++ src/renderer/src/pages/document.tsx
@@ -1,10 +1,27 @@
-import { Link } from "react-router-dom";
+import { ToC } from '../components/ToC'
 
-export default function Blank() {
+export default function Document() {
   return (
-    <main className="flex-1 flex items-center justify-center text-rotion-400">
-    Documento
-    <Link to="/">Voltar</Link>
+    <main className="flex-1 flex py-12 px-10 gap-8">
+      <aside className="hidden lg:block sticky top-0">
+        <span className="text-rotion-300 font-semibold text-xs">
+          TABELA DE CONTEÚDO
+        </span>
+      <ToC.Root>
+        <ToC.Link> 
+          Back-end
+        </ToC.Link>
+        <ToC.Section>
+        <ToC.Link>
+          Introdução
+          </ToC.Link> 
+          <ToC.Link>
+            Introdução
+            </ToC.Link> 
+        </ToC.Section>
+      </ToC.Root>
+      </aside>
+      <section className='flex-1 flex flex-col items-center'></section>
   </main>
   )
   }
